d37653193909f78f477b25d42a7e6e884801fb51
Vishal <vishalyadav1746@gmail.com>
2020-12-10 00:46:00 +0530

    add producer & consumer

---
+++ src/main/java/org/fretron/consumer/Consumer.java
@@ -0,0 +1,36 @@
+package org.fretron.consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("group.id", "test-group");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        List<String> topics = new ArrayList<>();
+        topics.add("firstTopic");
+        kafkaConsumer.subscribe(topics);
+        try {
+            do {
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(10);
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println("Topic - "+ record.topic() +", "+record.partition()+", Partition - %d, Value: "+record.value());
+                }
+            } while (true);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            kafkaConsumer.close();
+        }
+    }
+}
