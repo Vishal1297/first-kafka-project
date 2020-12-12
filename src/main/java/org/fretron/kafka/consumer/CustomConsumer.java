c34c69916f033f84a572f75c0639d3364f018fb5
Vishal <vishalyadav1746@gmail.com>
2020-12-12 22:47:01 +0530

    use default constant

--- src/main/java/org/fretron/kafka/consumer/CustomConsumer.java
+++ src/main/java/org/fretron/kafka/consumer/CustomConsumer.java
@@ -2,6 +2,7 @@ package org.fretron.kafka.consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.fretron.kafka.constant.Constant;
 import org.fretron.kafka.deserializer.CustomDeserializer;
 import org.fretron.kafka.model.User;
@@ -15,7 +16,7 @@ public class CustomConsumer {
     public static KafkaConsumer<String, User> createConsumer() {
         Properties properties = new Properties();
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, Constant.KAFKA_BROKERS);
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, Constant.KAFKA_DESERIALIZER);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, CustomDeserializer.class.getName());
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, Constant.KAFKA_GROUP_ID_CONFIG);
         properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
--- src/main/java/org/fretron/kafka/producer/CustomProducer.java
+++ src/main/java/org/fretron/kafka/producer/CustomProducer.java
@@ -2,6 +2,7 @@ package org.fretron.kafka.producer;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.fretron.kafka.constant.Constant;
 import org.fretron.kafka.model.User;
 import org.fretron.kafka.serializer.CustomSerializer;
@@ -13,7 +14,7 @@ public class CustomProducer {
     public static KafkaProducer<String, User> createProducer() {
         Properties properties = new Properties();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, Constant.KAFKA_BROKERS);
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, Constant.KAFKA_SERIALIZER);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CustomSerializer.class.getName());
         return new KafkaProducer<>(properties);
     }
